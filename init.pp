c663bbe1d64d5b7da8a803187b1b92176e2f482c
patilpramod <patilpramod09@gmail.com>
2021-05-14 22:55:43 +0530

    adding common

---
+++ init.pp
@@ -0,0 +1,24 @@
+class common_scripts{
+
+  if $fact[os].[family] == 'windows':{
+
+    $common_scripts_dir = "C:\\common_scripts"
+    file { $common_scripts_dir:
+            ensure => 'directory',
+            owner   => 'Administrator',
+            group   => 'Administrators',
+        }
+
+    ['get-availablePatches.ps1'].each | String $file | {
+          file { "${common_scripts_dir}\\${file}" :
+            ensure  => 'present',
+            owner   => 'Administrator',
+            group   => 'Administrators',
+            source  => "puppet:///modules/common_scripts/windows/${file}",
+            replace => true,
+            force   => true,
+        }
+    }
+
+  }
+}
